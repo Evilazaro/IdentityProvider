751ccb144b40e1f44c47b22cf96b6acccbcf7924
Evilázaro Alves <evilazaro@gmail.com>
2025-03-10 17:13:00 -0500

    new app environment

--- src/IdentityProvider.AppHost/Program.cs
+++
@@ -1,4 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.Identity_IdentityProvider>("identityprovider","https").WithExternalHttpEndpoints();
-builder.Build().Run();
---
+++ src/IdentityProvider/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityProvider.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
