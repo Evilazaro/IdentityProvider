b1727b5f66fe78c42b488bf8233012803c4c4930
Evilázaro Alves <evilazaro@gmail.com>
2025-03-10 11:34:12 -0500

    new service name

--- src/IdentityProvider.AppHost/Program.cs
+++ src/IdentityProvider.AppHost/Program.cs
@@ -1,5 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Identity_IdentityProvider>("identity-identityprovider");
+builder.AddProject<Projects.Identity_IdentityProvider>("identityprovider");
 
 builder.Build().Run();
