fad75d6ceb7b50a7cc41747bac97208d81e3fbc2
Evilázaro Alves <evilazaro@gmail.com>
2025-06-05 13:13:48 -0500

    Add AppRegistration C# Class

---
+++ src/IdentityProvider/Components/AppRegistration.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IdentityProvider.Components
+{
+    [Table("AppRegistrations")]
+    public class AppRegistration
+    {
+        [Key]
+        [Required]
+        [MaxLength(100)]
+        public required string ClientId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public required string ClientSecret { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public required string TenantId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public required string RedirectUri { get; set; }
+
+        [Required]
+        public required string Scopes { get; set; } // Comma-separated
+
+        [Required]
+        [MaxLength(200)]
+        public required string Authority { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public required string AppName { get; set; }
+
+        [MaxLength(500)]
+        public string? AppDescription { get; set; }
+
+        [Required]
+        public required string GrantTypes { get; set; } // Comma-separated
+
+        [Required]
+        public required string ResponseTypes { get; set; } // Comma-separated
+    }
+}
